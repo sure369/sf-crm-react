8f5dbd4cbff04683863262dbe303b20eb9735f41
sure369 <sureshmahath@gmail.com>
2023-01-04 15:50:36 +0530

    iframe test

--- crm-project/src/scenes/fileUpload/index.jsx
+++ crm-project/src/scenes/fileUpload/index.jsx
@@ -221,9 +221,8 @@ const DropFileInput = () => {
 
                                 {
                                     showIframe && 
-                                    <iframe src=
-"http://localhost:4000/2022-12-30T09-11-59.094Z-datcrmcsv.csv"></iframe>
-                                    // <iframe src="https://www.youtube.com/embed/s4BibernJxU"></iframe>
+                                    // <iframe src="http://localhost:4000/2022-12-30T09-11-59.094Z-datcrmcsv.csv"></iframe>
+                                    <iframe src="https://www.youtube.com/embed/s4BibernJxU"></iframe>
                                 }
                                 
         </Grid>
@@ -242,10 +241,10 @@ const DropFileInput = () => {
                       onClick={(e) =>
                         {
                           
-                            <iframe src=
-                            "http://localhost:4000/uploads/2022-12-30T09-11-59.094Z-datcrmcsv.csv"></iframe>
+                            // <iframe src=
+                            // "http://localhost:4000/uploads/2022-12-30T09-11-59.094Z-datcrmcsv.csv"></iframe>
 
-                            // saveAs (item.filePath, 'test.pdf')
+                            saveAs (item.filePath, 'test.pdf')
 
                         //    redirect(item);
                             // saveAs(item.filePath, `${item.fileName}.${item.fileType}`)
